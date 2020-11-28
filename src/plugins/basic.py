61f26924ef9fa3e686326237d06fb48989b3845d
DeltaZero <233a344a455@gmail.com>
2020-11-28 16:34:21 +0800

    Optimized file structure & add setup notification

--- src/plugins/basic.py
+++ src/plugins/basic.py
@@ -2,48 +2,31 @@ from nonebot import on_command, CommandSession
 import nonebot
 import time
 import random
+import asyncio
+
+async def __get_formatted_time():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 
-@on_command('TLOR', aliases=('T.L.O.R', 'tlor', 'prpr'))
-async def tlor(session: CommandSession):
+
+@nonebot.on_websocket_connect
+async def on_connected(event):
+    await asyncio.sleep(1)
+    await nonebot.helpers.send_to_superusers(nonebot.get_bot(), "DeltaBot has been started. %s"
+                                             %(await __get_formatted_time()))
+
+
+
+@on_command('~')
+async def interesting(session: CommandSession):
     msg = ['T^T', '~~~', 'Σ( ° △ °|||)︴', 'φ(≧ω≦*)♪', 'Ｏ(≧口≦)Ｏ']
     await session.send(random.choice(msg))
 
 
 
 @on_command('ping', aliases=('test', 'Test'))
-async def test(session: CommandSession):
-    t_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    await session.send("Pong! %s" %t_now)
-
-
-
-@on_command('num2word', aliases=('n2w', 'numtoword'))
-async def num2word(session: CommandSession):
-    num = int(session.get('num', prompt="Please input an integer between 0 and 10."))
-    word_list = "零一二三四五六七八九十"
-    await session.send(word_list[num])
-
-@num2word.args_parser
-async def _(session: CommandSession):
-    # 去掉消息首尾的空白符
-    stripped_arg = session.current_arg_text.strip()
-
-    if session.is_first_run:
-        # 该命令第一次运行（第一次进入命令会话）
-        if stripped_arg:
-            # 第一次运行参数不为空，意味着用户直接将城市名跟在命令名后面，作为参数传入
-            # 例如用户可能发送了：天气 南京
-            session.state['num'] = stripped_arg
-        return
-
-    if not stripped_arg:
-        # 发送了空白字符，则提示重新输入
-        # 这里 session.pause() 将会发送消息并暂停当前会话（该行后面的代码不会被运行）
-        session.pause('Input invalid. Please input again.')
-
-    # 如果当前正在向用户询问更多信息（例如本例中的要查询的城市），且用户输入有效，则放入会话状态
-    session.state[session.current_key] = stripped_arg
+async def ping(session: CommandSession):
+    await session.send("Pong! %s" %(await __get_formatted_time()))
 
 
 
@@ -51,7 +34,8 @@ async def _(session: CommandSession):
 async def log(session: CommandSession):
     msg = session.get('msg', prompt="Please enter a log message.")
     await session.send("Send msg to log successfully.")
-    print("Log: %s" %msg)
+    print(msg)
+    # print("Log: %s" %msg)
 
 @log.args_parser
 async def _(session: CommandSession):
