83ac006661ee4160b69c900c6b672a5da1d17a4a
DeltaZero <233a344a455@gmail.com>
2020-12-13 10:02:38 +0800

    Add xml plugin

--- src/plugins/xml.py
+++ src/plugins/xml.py
@@ -1,29 +1,31 @@
-# import nonebot
-# import time
-# from nonebot import on_command, CommandSession, message
-#
-# __plugin_name__ = 'xml'
-# __plugin_usage__ = r"""
-# Transform xml code into a xml card.
-# Command(s):
-#  - /xml [code]
-# """.strip()
-#
-#
-# @on_command('xml', aliases=('toxml', 'to_xml'))
-# async def xml(session: CommandSession):
-#     pass
-#
-# @xml.args_parser
-# async def _(session: CommandSession):
-#     arg = session.current_arg
-#
-#     if session.is_first_run:
-#         if arg:
-#             session.state['code'] = arg
-#         return
-#
-#     if not arg:
-#         session.pause('Please input the xml code.')
-#
-#     session.state[session.current_key] = arg
+from nonebot import on_command, CommandSession, message
+
+__plugin_name__ = 'xml'
+__plugin_usage__ = r"""
+Transform xml code into a XML card.
+*Notice that excessive XML msg may cause your account frozen!*
+Command(s):
+ - /xml [code]
+""".strip()
+
+def get_xml_segment(data: str) -> message.MessageSegment:
+    return message.MessageSegment(type_='xml', data={'data': str(data)})
+
+@on_command('xml', aliases=('toxml', 'to_xml'))
+async def xml(session: CommandSession):
+    data = session.get('data', prompt="Please input xml data.")
+    await session.send(get_xml_segment(data))
+
+@xml.args_parser
+async def _(session: CommandSession):
+    arg = session.current_arg
+
+    if session.is_first_run:
+        if arg:
+            session.state['data'] = arg
+        return
+
+    if not arg:
+        session.pause('Please input the xml data.')
+
+    session.state[session.current_key] = arg
