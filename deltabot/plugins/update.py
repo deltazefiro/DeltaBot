4c543d05daac45c1c4eb0cde43bf88b3119c9c48
DeltaZero <deltazefiro@gmail.com>
2021-01-01 09:20:02 +0800

    Add update plugin

---
+++ deltabot/plugins/update.py
@@ -0,0 +1,25 @@
+from nonebot import CommandSession, on_command, permission
+from loguru import logger
+from subprocess import PIPE, STDOUT, CalledProcessError
+import subprocess
+
+__plugin_name__ = '[A]update'
+__plugin_usage__ = r"""
+从Git上更新DeltaBot
+【实验性功能，可能意外损坏DeltaBot，请谨慎使用】
+【需要管理员权限】
+""".strip()
+
+@on_command('update', permission=permission.SUPERUSER)
+async def update(session: CommandSession):
+    logger.warning("Start pulling from git")
+    await session.send("Start pulling from git")
+    try:
+        process = subprocess.run(['git', 'pull'], stdout=PIPE, stderr=STDOUT)
+
+        logger.info(process.stdout.decode('utf-8'))
+        await session.send(process.stdout.decode('utf-8'))
+
+    except CalledProcessError as e:
+        logger.error(f"Fail to update: {e}")
+        await session.send(f"Fail to update: {e}")
