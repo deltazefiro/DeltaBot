b54a0bf39c6eac6d60d65fdf3da3fc49ff4eef8d
DeltaZero <deltazefiro@gmail.com>
2021-01-08 19:29:31 +0800

    Fix 'reload' plugin (#3)

--- deltabot/plugins/reload.py
+++ deltabot/plugins/reload.py
@@ -1,38 +1,36 @@
-from nonebot import on_command, CommandSession, permission
+import sys
+from os import path
+
 import nonebot
-import os
+from loguru import logger
+from nonebot import on_command, CommandSession, permission, get_bot
+
+from .. import config
 
 __plugin_name__ = '[A]reload'
 __plugin_usage__ = r"""
-Reload all plugins.
-*Required admin permission*
+重载配置文件和所有插件
+【需要管理员权限】
 Command(s):
  - /reload
 """.strip()
 
-PLUGINS_PATH = "."
-PLUGINS_PATH = os.path.join(os.path.dirname(__file__), PLUGINS_PATH)
-
 @on_command('reloadplugins', aliases=('reloadplugin', 'reload'), permission=permission.SUPERUSER)
 async def reloadplugins(session: CommandSession):
-    for f in os.listdir(PLUGINS_PATH):
-        if f != '__pycache__' and f != 'reload.py' and f[0] != '_':
-            plugin_path = 'deltabot.plugins.' + os.path.splitext(f)[0]
-            ret = nonebot.plugin.reload_plugin(plugin_path)
-            if not ret:
-                ret = nonebot.plugin.load_plugin(plugin_path)
-            if not ret:
-                await session.send("[WARNING] Failed to reload '%s' plugin!" %plugin_path)
-    await session.send("Plugins reloaded.")
+    # Reload config
+    get_bot().config = config
+    logger.info("Config reloaded.")
 
+    # Reload plugins
+    plugins = nonebot.plugin.get_loaded_plugins()
+    for plugin in plugins:
+        module_path = plugin.module.__name__
+        nonebot.plugin.PluginManager.remove_plugin(module_path)
+        for module in list(
+                filter(lambda x: x.startswith(module_path), sys.modules.keys())):
+            del sys.modules[module]
 
+    reloaded_plugins = nonebot.load_plugins(path.dirname(__file__), 'deltabot.plugins')
+    logger.info(f"{len(reloaded_plugins)} plugin(s) reloaded.")
+    await session.send(f"已成功重载{len(reloaded_plugins)}个插件")
 
-# @on_command('reloadrunningplugins', aliases=('reloadrplugin', 'reload'))
-# async def reload_running_plugins(session: CommandSession):
-#     plugins = nonebot.plugin.get_loaded_plugins()
-#     for plugin in plugins:
-#         plugin_path = plugin.module.__name__
-#         # await session.send("Reloading %s ..." %plugin_path)
-#         if plugin_path != 'plugin.reload':
-#             nonebot.plugin.reload_plugin(plugin_path)
-#     await session.send("Reloaded %d plugins successfully." %(len(plugins)-1))
